2d3c707fb6b8aec0fcec4e9f6bcfc953c00e8b41
SIPU <ddasik00@naver.com>
2019-02-24 15:20:31 +0900

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,9 +8,6 @@ fn main() {
             let loadfile = &args[1];
             let savefile = &args[2];
 
-            let con  = loadfilef(loadfile.to_string());
-            count_word(&con, "테스트");
-            savefilef(savefile.to_string(), con);
         },
         // all the other cases
         _ => {
@@ -24,48 +17,9 @@ fn main() {
     }
 }
 
-fn count_word (content :&String, word : &str) {
-    let leng = content.split(word).count() - 1;
-    println!("Tested : {:?}",leng);
-}
-
-fn loadfilef (filename:String) -> String {
-    let path = Path::new(&filename);
-    let display = path.display();
-    let mut file = match File::open(&path) {
-        Err(e) => panic!("Open error {}:{}", display, e.description()),
-        Ok(file) => file,
-    };
-
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Err(e) => panic!("Read error {} : {}", display, e.description()),
-        Ok(_) => println!("Load Complete."),
-    };
-    contents
-}
-
-fn savefilef (filename:String, content:String) {
-    let path = Path::new(&filename);
-    let display = path.display();
-
-    let mut file = match File::create(&path) {
-    Err(e) => panic!("couldn't create {}: {}",
-                        display,
-                        e.description()),
-    Ok(file) => file,
-    };
-
-    match file.write_all(content.as_bytes()) {
-        Err(e) => {
-            panic!("couldn't write to {}: {}", display,
-                                               e.description())
-        },
-        Ok(_) => println!("successfully wrote to {}", display),
-    }
-}
-
 fn help () {
-    println!("please match arguments");
+    println!("run as:");
+    println!("rhetoric filename1.txt filename2.txt keyword.txt");
+    println!(":)");
 }
 
